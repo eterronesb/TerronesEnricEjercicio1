7796a306710c7c143ee86d53c360c90a29ffacd5
eterronesb <sucir1993@gmail.com>
2024-12-08 11:45:42 +0100

    Creacion de mi perfil e inicio del menu para el usuario

--- src/ApellidoNombreMain.java
+++ src/ApellidoNombreMain.java
@@ -1,6 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ApellidoNombreMain {
     public static void main(String[] args) {
 
-
+    ApellidoNombrePerfil myProfile = new ApellidoNombrePerfil(
+        "Enric Terrones Barceiro",
+        "Soy estudiante de DAW y trabajo en un call center",
+            "Gimnasio, videojuegos, Futbol",
+            "Sushi, pizza, Paella",
+            "He recorrido casi todo Portugal, Puedo tener una cancion todo un dia en bucle, Como pan con la pasta"
+            );
+    Scanner scanner = new Scanner(System.in);
+    int option;
+    do {
+        System.out.println("\nMenu AboutMe [" +myProfile.getName() + "]");
+        System.out.println("[1] Story");
+        System.out.println("[2] Favorites");
+        System.out.println("[3] Fun Fact");
+        System.out.println("[4] Salir");
+    }
     }
 }
--- src/ApellidoNombrePerfil.java
+++ src/ApellidoNombrePerfil.java
@@ -34,5 +34,4 @@ public class ApellidoNombrePerfil {
         return funFacts;
     }
 
-
 }
